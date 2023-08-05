60a682b0a34ca67e976f724f3d92bff705871d83
Yuri Gomes <yuurig@gmail.com>
2023-08-05 14:45:52 -0300

    :fire: chore: init studies

---
+++ src/main/kotlin/br/com/met4tron/variables/types.kt
@@ -0,0 +1,15 @@
+package br.com.met4tron.variables
+
+fun main() {
+    /**
+     *     Numbers and their unsigned counterparts
+     *      - double
+     *      - int
+     *      - decimal
+     *      - hexadecimal
+     *     Booleans
+     *     Characters
+     *     Strings
+     *     Arrays
+     */
+}
